df81eeabb4cc3d6f6a76d5257183a685964e4cb3
Hyj <2856042536@qq.com>
2024-02-26 22:10:28 +0800

    第二次提交

--- src/main/java/com/hyj/git/GitTest.java
+++ src/main/java/com/hyj/git/GitTest.java
@@ -2,7 +2,7 @@ package com.hyj.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello GitHub!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
